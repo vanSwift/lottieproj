5025464a36a0da055c4f439dfd7df6004f111971
Ivan Terekhov <terekhovvan@yahoo.com>
2025-01-09 12:44:03 +0200

    firstcommit

---
+++ lottieproj/lottieprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  lottieprojApp.swift
+//  lottieproj
+//
+//  Created by Иван Терехов on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct lottieprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
