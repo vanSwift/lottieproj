6c3d76dcb46932d9267efa7f8702d0f988cc140a
Ivan Terekhov <terekhovvan@yahoo.com>
2025-01-09 13:51:02 +0200

    added animation

--- lottieproj/ContentView.swift
+++ lottieproj/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
     var body: some View {
@@ -14,10 +15,42 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            LottieAnimation()
         }
         .padding()
     }
 }
+
+struct LottieAnimation: UIViewRepresentable{
+    
+    let name: String = "bricks"
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+        let view = UIView()
+        let animationView = LottieAnimationView(name: name)
+        animationView.loopMode = .autoReverse
+        animationView.play()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+        
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        let view = UIView()
+        
+    }
+    
+}
+
 
 #Preview {
     ContentView()
